d3833c57be3d3a744733b704be8e40d2d22d85db
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2024-12-23 18:14:09 +0100

    Add support for writing png/jpeg

--- src/rendering/floatmap.rs
+++ src/rendering/floatmap.rs
@@ -4,8 +4,19 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::{fs::File, io::BufWriter};
 
+use image::RgbImage;
+
 pub struct FloatMap {
     pub map: gridmap::GridMap<f32>,
+}
+
+fn color_for_occ(occ: f32) -> [u8; 3] {
+    let c = 255. - 255. * occ;
+    match occ {
+        -1. => [140, 170, 238],
+        -2. => [231, 130, 132],
+        _ => [c as u8; 3],
+    }
 }
 
 impl FloatMap {
@@ -29,16 +40,27 @@ impl FloatMap {
             let y = self.map.size[1] - i;
             for x in 0..self.map.size[0] {
                 let occ = self.map.cell(x as i32, y as i32).unwrap_or(&default_cell);
-                let c = 255. - 255. * occ;
-                let rgb: [u8; 3] = match occ {
-                    -1. => [140, 170, 238],
-                    -2. => [231, 130, 132],
-                    _ => [c as u8; 3],
-                };
+                let rgb = color_for_occ(*occ);
                 writer.write_all(&rgb)?;
             }
         }
 
         Ok(())
+    }
+
+    pub fn to_image(&self) -> RgbImage {
+        let mut img = RgbImage::new(self.map.size[0] as u32, self.map.size[1] as u32);
+
+        let default_cell = -1.0f32;
+        for y in 0..self.map.size[1] {
+            let ty = self.map.size[1] - y - 1;
+            for x in 0..self.map.size[0] {
+                let occ = self.map.cell(x as i32, y as i32).unwrap_or(&default_cell);
+                let pixel = img.get_pixel_mut(x as u32, ty as u32);
+                *pixel = image::Rgb(color_for_occ(*occ));
+            }
+        }
+
+        img
     }
 }
