0ceb11a7175c1fe89bea83345f1ec31d31f2959a
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2024-12-23 10:52:34 +0100

    Fix boundaries calculation

--- src/rendering/boundaries.rs
+++ src/rendering/boundaries.rs
@@ -1,11 +1,9 @@
 extern crate nalgebra as na;
+use na::Point2;
+
 use crate::datastream::robot_data::RobotLaser;
 
-fn update_min_max(
-    point: &na::Vector2<f64>,
-    min: &mut na::Vector2<f64>,
-    max: &mut na::Vector2<f64>,
-) {
+fn update_min_max(point: &na::Point2<f64>, min: &mut na::Vector2<f64>, max: &mut na::Vector2<f64>) {
     min.x = min.x.min(point.x);
     min.y = min.y.min(point.y);
     max.x = max.x.max(point.x);
@@ -23,7 +21,7 @@ pub fn boundaries(
     let tp = offset * scan.laser_pose();
 
     // consider robot pose;
-    update_min_max(&tp.translation.vector, min, max);
+    update_min_max(&Point2::from(tp.translation.vector), min, max);
     let max_range = (scan.laser_params.max_range as f32).min(max_range.unwrap_or(f32::INFINITY));
     for (i, range) in scan.ranges.iter().enumerate() {
         let mut r = *range;
@@ -33,7 +31,7 @@ pub fn boundaries(
         if usable_range.is_some() {
             r = r.min(usable_range.unwrap());
         }
-        let point = na::Vector2::new(r as f64, 0.);
+        let point = na::Point2::new(r as f64, 0.);
         let transformed_point = tp * scan.laser_params.beam_isometry(i) * point;
         update_min_max(&transformed_point, min, max);
     }
