bd94ef0fff312eb0b0b7c10b8e0c8ece107b149e
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2024-12-24 15:00:35 +0100

    Add option to draw the robot's path

--- src/rendering/floatmap.rs
+++ src/rendering/floatmap.rs
@@ -1,63 +1,28 @@
 use super::gridmap;
 
-use std::io::Write;
-use std::path::PathBuf;
-use std::{fs::File, io::BufWriter};
-
-use image::RgbImage;
-
 pub struct FloatMap {
     pub map: gridmap::GridMap<f32>,
 }
 
-fn color_for_occ(occ: f32) -> [u8; 3] {
-    let c = 255. - 255. * occ;
+fn color_for_occ(occ: f32) -> [u8; 4] {
+    let c = (255. - 255. * occ) as u8;
     match occ {
-        -1. => [140, 170, 238],
-        -2. => [231, 130, 132],
-        _ => [c as u8; 3],
+        -1. => [140, 170, 238, 255],
+        _ => [c, c, c, 255],
     }
 }
 
 impl FloatMap {
-    pub fn save_as_ppm(&self, filename: &PathBuf) -> std::io::Result<()> {
-        let file = File::create(filename)?;
-        let mut writer = BufWriter::new(file);
+    pub fn to_pixels(&self) -> Vec<u8> {
+        let capacity = self.map.size[0] * self.map.size[1] * 4;
+        let mut img = Vec::with_capacity(capacity);
 
-        writeln!(writer, "P6")?;
-        writeln!(writer, "# log2gfx")?;
-        writeln!(writer, "# resolution {}", self.map.resolution)?;
-        writeln!(
-            writer,
-            "# offset {} {}",
-            self.map.offset.x, self.map.offset.y
-        )?;
-        writeln!(writer, "{} {} 255", self.map.size[0], self.map.size[1])?;
-
-        // Write the image content
         let default_cell = -1.0f32;
         for i in 1..=self.map.size[1] {
             let y = self.map.size[1] - i;
             for x in 0..self.map.size[0] {
                 let occ = self.map.cell(x as i32, y as i32).unwrap_or(&default_cell);
-                let rgb = color_for_occ(*occ);
-                writer.write_all(&rgb)?;
-            }
-        }
-
-        Ok(())
-    }
-
-    pub fn to_image(&self) -> RgbImage {
-        let mut img = RgbImage::new(self.map.size[0] as u32, self.map.size[1] as u32);
-
-        let default_cell = -1.0f32;
-        for y in 0..self.map.size[1] {
-            let ty = self.map.size[1] - y - 1;
-            for x in 0..self.map.size[0] {
-                let occ = self.map.cell(x as i32, y as i32).unwrap_or(&default_cell);
-                let pixel = img.get_pixel_mut(x as u32, ty as u32);
-                *pixel = image::Rgb(color_for_occ(*occ));
+                img.extend(color_for_occ(*occ));
             }
         }
 
